420332a107387b16260edca6df5b375d56ca5129
Tin Nguyen <tinitgithub@gmail.com>
2023-07-31 01:51:24 +0700

    Fix

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -89,6 +89,7 @@ export class UserService {
       .cookie('jwt', jwt, {
         expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
         sameSite: 'none',
+        signed: true,
         secure: false,
         domain: '.vercel.app',
       })
