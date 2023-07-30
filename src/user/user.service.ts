3330748e03b33236995930df5a0e31169b59491b
Tin Nguyen <tinitgithub@gmail.com>
2023-07-30 22:16:34 +0700

    Fix

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -98,6 +98,7 @@ export class UserService {
         sameSite: 'none',
         secure: true,
         httpOnly: true,
+        domain: 'lookup-app-backend.vercel',
       })
       .json({
         user,
