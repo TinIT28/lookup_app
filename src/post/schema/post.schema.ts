f1f351b65f17c1ac3fa4c280a7671b92abbd4ded
Tin Nguyen <tinitgithub@gmail.com>
2023-07-30 15:26:53 +0700

    Fix

--- src/post/schema/post.schema.ts
+++ src/post/schema/post.schema.ts
@@ -1,34 +1,31 @@
-import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
-import * as mongoose from 'mongoose';
-import { User } from "src/user/schema/user.schema";
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { User } from 'src/user/schema/user.schema';
+import mongoose = require('mongoose');
 
 @Schema({
-    timestamps: true
+  timestamps: true,
 })
 export class Posts {
+  @Prop()
+  content: string;
 
-    @Prop()
-    content: string;
+  @Prop()
+  images: string[];
 
-    @Prop()
-    images: string[];
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  user: any;
 
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
-    user: any;
+  @Prop({ type: [String], default: [] })
+  likes: string[];
 
-    @Prop({ type: [String], default: [] })
-    likes: string[];
-
-    @Prop([
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Comment',
-            default: [],
-        },
-    ])
-    comments: any[];
-
-
+  @Prop([
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment',
+      default: [],
+    },
+  ])
+  comments: any[];
 }
 
 export const PostsSchema = SchemaFactory.createForClass(Posts);
