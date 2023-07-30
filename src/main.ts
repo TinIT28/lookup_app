4b803804aba578f49371d5e3ba99e67f6a08c436
Tin Nguyen <tinitgithub@gmail.com>
2023-07-30 15:19:16 +0700

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -8,26 +8,26 @@ import passport = require('passport');
 import cookieParser = require('cookie-parser');
 import bodyParser = require('body-parser');
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      "https://lookup-app-frontend.vercel.app",
-      "http://localhost:3000"
-    ]
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
   });
-  app.use(session({
-    secret: 'jfaejlfhsdhafddksf',
-    saveUninitialized: false,
-    resave: false,
-    cookie: {
-      maxAge: 60000,
-    }
-  }));
-  app.use(bodyParser.json({ limit: "20mb" }));
+  app.use(
+    session({
+      secret: 'jfaejlfhsdhafddksf',
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        maxAge: 60000,
+      },
+    }),
+  );
+  app.use(bodyParser.json({ limit: '20mb' }));
   app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
-  app.use(cookieParser())
+  app.use(cookieParser());
   app.use(passport.initialize());
   app.use(passport.session());
   app.useWebSocketAdapter(new IoAdapter(app));
