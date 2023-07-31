09527d00b7f3fba5450545889df6b9cd93a55a56
Tin Nguyen <tinitgithub@gmail.com>
2023-07-31 14:57:41 +0700

    Fix provice api

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
       'https://provinces.open-api.vn',
     ],
     credentials: true,
+    methods: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
   });
   app.use(
     session({
